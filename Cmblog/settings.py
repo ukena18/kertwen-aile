a37e2ef75911e04f351f97b88eb1b738c189a18a
ukena18 <ukena18@gmail.com>
2021-12-14 21:11:22 -0700

    add changes for user_table

--- Cmblog/settings.py
+++ Cmblog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import storages.backends.s3boto3
@@ -164,3 +164,6 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+django_heroku.settings(locals())
