25579d955f92553f785f140b5e953137026e2377
ukena18 <ukena18@gmail.com>
2022-11-03 13:45:42 -0600

    clean the code

--- blog/views.py
+++ blog/views.py
@@ -16,20 +16,20 @@ from django.views.generic import (ListView,
 
 from .forms import CommentForm
 
->>> from django.core.paginator import Paginator
->>> posts=['1','2','3','4','5']
->>> p = Paginator(posts,2)
->>> p1 = p.page(1)
->>> p1
-<Page 1 of 3>
->>> p1.object_list
-['1', '2']
->>> p1.has_previous()
-False
->>> p1.has_next()
-True
->>> p1.next_page_number()
-2
+# >>> from django.core.paginator import Paginator
+# >>> posts=['1','2','3','4','5']
+# >>> p = Paginator(posts,2)
+# >>> p1 = p.page(1)
+# >>> p1
+# <Page 1 of 3>
+# >>> p1.object_list
+# ['1', '2']
+# >>> p1.has_previous()
+# False
+# >>> p1.has_next()
+# True
+# >>> p1.next_page_number()
+# 2
 
 
 
