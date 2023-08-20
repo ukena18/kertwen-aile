85dd5eb8d643f76d3f81adc33e5f48fbc6875713
ukena18 <92485052+ukena18@users.noreply.github.com>
2023-08-20 16:08:20 -0600

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks. xjdnc"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cmblog.settings')
     try:
         from django.core.management import execute_from_command_line
